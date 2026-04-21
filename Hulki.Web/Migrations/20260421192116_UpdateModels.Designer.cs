70b85e412990785ed7b61a1f087fcbbbc5d62eb9
Kacper Strześniewski <kacper.strzesniewski@o2.pl>
2026-04-21 21:28:37 +0200

    secrets

--- Hulki.Web/Migrations/20260421192116_UpdateModels.Designer.cs
+++ Hulki.Web/Migrations/20260421192116_UpdateModels.Designer.cs
@@ -4,6 +4,7 @@ using Hulki.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hulki.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421192116_UpdateModels")]
+    partial class UpdateModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
