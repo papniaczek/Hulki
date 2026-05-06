ec9bc6203914e87dab5726135e9827e3bce06fed
papniaczek <kacper.strzesniewski@o2.pl>
2026-05-06 15:04:54 +0200

    added more game types

--- Hulki.Web/Controllers/StoreController.cs
+++ Hulki.Web/Controllers/StoreController.cs
@@ -7,125 +7,198 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Hulki.Web.Controllers;
 
-[Authorize] // Tylko zalogowani
-    public class StoreController : Controller
+[Authorize]
+public class StoreController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<AppUser> _userManager;
+
+    public StoreController(ApplicationDbContext context, UserManager<AppUser> userManager)
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<AppUser> _userManager;
+        _context = context;
+        _userManager = userManager;
+    }
 
-        public StoreController(ApplicationDbContext context, UserManager<AppUser> userManager)
+    public async Task<IActionResult> Index()
+    {
+        await SeedStoreDataIfNotExists();
+
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
+        
+        ViewBag.Points = wallet?.Balance ?? 0;
+
+        var games = await _context.Games.Include(g => g.GameType).ToListAsync();
+        return View(games);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PlayRoulette(Guid gameId)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
+        var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == gameId);
+
+        if (wallet == null || game == null || wallet.Balance < game.Cost)
         {
-            _context = context;
-            _userManager = userManager;
-        }
-
-        // 1. WYŚWIETLENIE SKLEPU
-        public async Task<IActionResult> Index()
-        {
-            await SeedStoreDataIfNotExists();
-
-            var user = await _userManager.GetUserAsync(User);
-            var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
-            
-            ViewBag.Points = wallet?.Balance ?? 0;
-
-            var games = await _context.Games.Include(g => g.GameType).ToListAsync();
-            return View(games);
-        }
-
-        // 2. OTWIERANIE SKRZYNKI
-        [HttpPost]
-        public async Task<IActionResult> OpenLootbox(Guid gameId)
-        {
-            var user = await _userManager.GetUserAsync(User);
-            var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
-            var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == gameId);
-
-            if (wallet == null || game == null || wallet.Balance < game.Cost)
-            {
-                TempData["ErrorMessage"] = "Nie masz wystarczająco punktów, aby otworzyć tę skrzynkę!";
-                return RedirectToAction("Index");
-            }
-
-            wallet.Balance -= game.Cost;
-            _context.PointTransactions.Add(new PointTransaction 
-            { 
-                Id = Guid.NewGuid(), Amount = -game.Cost, 
-                Description = $"Otwarcie skrzynki: {game.Name}", 
-                TransactionDate = DateTime.Now, WalletId = wallet.Id 
-            });
-
-            // Losowanie nagrody
-            var allRewards = await _context.RewardItems.Include(r => r.ItemRarity).ToListAsync();
-            var random = new Random();
-            var wonItem = allRewards[random.Next(allRewards.Count)];
-
-            // ZABEZPIECZENIE PRZED DUPLIKATEM
-            bool alreadyOwns = await _context.PatientInventories.AnyAsync(pi => pi.AppUserId == user.Id && pi.RewardItemId == wonItem.Id);
-            
-            if (alreadyOwns)
-            {
-                int refund = game.Cost / 2;
-                wallet.Balance += refund;
-                _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = refund, Description = $"Duplikat: {wonItem.Name} (Zwrot pkt)", TransactionDate = DateTime.Now, WalletId = wallet.Id });
-                TempData["SuccessMessage"] = $"Wylosowano: {wonItem.Name} ({wonItem.ItemRarity.Name}), ale już to masz w ekwipunku! Otrzymujesz {refund} pkt zwrotu.";
-            }
-            else
-            {
-                _context.PatientInventories.Add(new PatientInventory { AppUserId = user.Id, RewardItemId = wonItem.Id });
-                TempData["SuccessMessage"] = $"Gratulacje! Wygrałeś nowy przedmiot: {wonItem.Name} [{wonItem.ItemRarity.Name}]!";
-            }
-
-            _context.GameSessions.Add(new GameSession { Id = Guid.NewGuid(), AppUserId = user.Id, GameId = game.Id, PlayedAt = DateTime.Now });
-
-            await _context.SaveChangesAsync();
+            TempData["ErrorMessage"] = "Nie masz wystarczająco punktów, aby otworzyć tę skrzynkę!";
             return RedirectToAction("Index");
         }
 
-        private async Task SeedStoreDataIfNotExists()
+        wallet.Balance -= game.Cost;
+        _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = -game.Cost, Description = $"Gra: {game.Name}", TransactionDate = DateTime.Now, WalletId = wallet.Id });
+
+        // Losowanie nagrody
+        var allRewards = await _context.RewardItems.Include(r => r.ItemRarity).ToListAsync();
+        var random = new Random();
+        var wonItem = allRewards[random.Next(allRewards.Count)];
+
+        // Zabezpieczenie przed duplikatem
+        bool alreadyOwns = await _context.PatientInventories.AnyAsync(pi => pi.AppUserId == user.Id && pi.RewardItemId == wonItem.Id);
+        if (alreadyOwns)
         {
-            if (!await _context.ItemRarities.AnyAsync())
-            {
-                _context.ItemRarities.AddRange(
-                    new ItemRarity { Name = "Zwykły", HexColor = "#808080" },
-                    new ItemRarity { Name = "Legendarny", HexColor = "#FFA500" }
-                );
-                await _context.SaveChangesAsync();
-            }
+            int refund = game.Cost / 2;
+            wallet.Balance += refund;
+            _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = refund, Description = $"Duplikat: {wonItem.Name} (Zwrot pkt)", TransactionDate = DateTime.Now, WalletId = wallet.Id });
+            ViewBag.Message = $"Wylosowano duplikat! Otrzymujesz {refund} pkt zwrotu.";
+        }
+        else
+        {
+            _context.PatientInventories.Add(new PatientInventory { AppUserId = user.Id, RewardItemId = wonItem.Id });
+            ViewBag.Message = $"Gratulacje! Wygrałeś nowy przedmiot: {wonItem.Name}!";
+        }
 
-            if (!await _context.GameTypes.AnyAsync())
-            {
-                _context.GameTypes.Add(new GameType { Name = "Lootbox", Description = "Skrzynia z losową zawartością" });
-                await _context.SaveChangesAsync();
-            }
+        _context.GameSessions.Add(new GameSession { Id = Guid.NewGuid(), AppUserId = user.Id, GameId = game.Id, PlayedAt = DateTime.Now });
+        await _context.SaveChangesAsync();
 
-            if (!await _context.RewardItems.AnyAsync())
-            {
-                var normal = await _context.ItemRarities.FirstOrDefaultAsync(r => r.Name == "Zwykły");
-                var legendary = await _context.ItemRarities.FirstOrDefaultAsync(r => r.Name == "Legendarny");
-                _context.RewardItems.AddRange(
-                    new RewardItem { Id = Guid.NewGuid(), Name = "Miedziana Odznaka Startu", ItemRarityId = normal.Id },
-                    new RewardItem { Id = Guid.NewGuid(), Name = "Tytuł: Wojownik Światła", ItemRarityId = normal.Id },
-                    new RewardItem { Id = Guid.NewGuid(), Name = "Legendarny Puchar Niezłomnych", ItemRarityId = legendary.Id }
-                );
-                await _context.SaveChangesAsync();
-            }
+        // Przekazanie danych do widoku Ruletki
+        ViewBag.WonItem = wonItem;
+        
+        // Przekazujemy uproszczoną listę wszystkich nagród do wygenerowania ruletki
+        ViewBag.AllRewards = allRewards.Select(r => new { 
+            name = r.Name, 
+            rarity = r.ItemRarity.Name 
+        }).ToList();
 
-            if (!await _context.Games.AnyAsync())
-            {
-                var lootboxType = await _context.GameTypes.FirstOrDefaultAsync(t => t.Name == "Lootbox");
-                _context.Games.Add(new Game 
-                { 
-                    Id = Guid.NewGuid(), 
-                    Name = "Skrzynia Motywacji", 
-                    Description = "Wydaj 10 punktów i spróbuj zdobyć legendarne wyposażenie do swojego profilu!", 
-                    Cost = 10, 
-                    GameTypeId = lootboxType.Id 
-                });
-                await _context.SaveChangesAsync();
-            }
+        return View("RouletteGame");
+    }
+
+    // --- NOWA GRA: 3 KARTY ---
+    [HttpPost]
+    public async Task<IActionResult> PlayCards(Guid gameId)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
+        var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == gameId);
+
+        if (wallet == null || game == null || wallet.Balance < game.Cost)
+        {
+            TempData["ErrorMessage"] = "Nie masz wystarczająco punktów!";
+            return RedirectToAction("Index");
+        }
+
+        // Płatność
+        wallet.Balance -= game.Cost;
+        _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = -game.Cost, Description = $"Gra: {game.Name}", TransactionDate = DateTime.Now, WalletId = wallet.Id });
+
+        // Losowanie 3 różnych przedmiotów
+        var allRewards = await _context.RewardItems.Include(r => r.ItemRarity).ToListAsync();
+        var random = new Random();
+        var shuffled = allRewards.OrderBy(x => random.Next()).Take(3).ToList();
+        
+        var wonItem = shuffled[0]; // To gracz "wygra"
+        var missed1 = shuffled[1]; // To pokaże się na innych kartach
+        var missed2 = shuffled[2];
+
+        // Zabezpieczenie przed duplikatem
+        bool alreadyOwns = await _context.PatientInventories.AnyAsync(pi => pi.AppUserId == user.Id && pi.RewardItemId == wonItem.Id);
+        if (alreadyOwns)
+        {
+            int refund = game.Cost / 2;
+            wallet.Balance += refund;
+            _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = refund, Description = $"Duplikat (Zwrot)", TransactionDate = DateTime.Now, WalletId = wallet.Id });
+            ViewBag.Message = $"Wylosowano duplikat! Otrzymujesz {refund} pkt zwrotu.";
+        }
+        else
+        {
+            _context.PatientInventories.Add(new PatientInventory { AppUserId = user.Id, RewardItemId = wonItem.Id });
+            ViewBag.Message = $"Gratulacje! Wygrałeś: {wonItem.Name}!";
+        }
+
+        _context.GameSessions.Add(new GameSession { Id = Guid.NewGuid(), AppUserId = user.Id, GameId = game.Id, PlayedAt = DateTime.Now });
+        await _context.SaveChangesAsync();
+
+        // Przekazanie danych do widoku "3 Kart"
+        ViewBag.WonItem = wonItem;
+        ViewBag.Missed1 = missed1;
+        ViewBag.Missed2 = missed2;
+
+        return View("CardsGame"); 
+    }
+
+    // --- NOWA GRA: ZDRAPKA ---
+    [HttpPost]
+    public async Task<IActionResult> PlayScratchcard(Guid gameId)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var wallet = await _context.Wallets.FirstOrDefaultAsync(w => w.AppUserId == user.Id);
+        var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == gameId);
+
+        if (wallet == null || game == null || wallet.Balance < game.Cost)
+        {
+            TempData["ErrorMessage"] = "Nie masz wystarczająco punktów!";
+            return RedirectToAction("Index");
+        }
+
+        wallet.Balance -= game.Cost;
+        _context.PointTransactions.Add(new PointTransaction { Id = Guid.NewGuid(), Amount = -game.Cost, Description = $"Gra: {game.Name}", TransactionDate = DateTime.Now, WalletId = wallet.Id });
+
+        var allRewards = await _context.RewardItems.Include(r => r.ItemRarity).ToListAsync();
+        var random = new Random();
+        var wonItem = allRewards[random.Next(allRewards.Count)];
+
+        bool alreadyOwns = await _context.PatientInventories.AnyAsync(pi => pi.AppUserId == user.Id && pi.RewardItemId == wonItem.Id);
+        if (alreadyOwns)
+        {
+            wallet.Balance += (game.Cost / 2);
+            ViewBag.Message = $"Duplikat. Zwrot {game.Cost / 2} pkt.";
+        }
+        else
+        {
+            _context.PatientInventories.Add(new PatientInventory { AppUserId = user.Id, RewardItemId = wonItem.Id });
+            ViewBag.Message = $"Nowy przedmiot w ekwipunku!";
+        }
+
+        _context.GameSessions.Add(new GameSession { Id = Guid.NewGuid(), AppUserId = user.Id, GameId = game.Id, PlayedAt = DateTime.Now });
+        await _context.SaveChangesAsync();
+
+        ViewBag.WonItem = wonItem;
+        return View("ScratchcardGame");
+    }
+
+    private async Task SeedStoreDataIfNotExists()
+    {
+        // ... (Tu zostaje Twój kod seedowania rzadkości i nagród) ...
+
+        if (!await _context.GameTypes.AnyAsync(t => t.Name == "3 Karty"))
+        {
+            _context.GameTypes.Add(new GameType { Name = "3 Karty", Description = "Wybierz jedną z 3 kart" });
+            _context.GameTypes.Add(new GameType { Name = "Zdrapka", Description = "Odkryj pole, aby sprawdzić nagrodę" });
+            await _context.SaveChangesAsync();
+        }
+
+        if (!await _context.Games.AnyAsync(g => g.Name == "Ślepy Los"))
+        {
+            var cardsType = await _context.GameTypes.FirstOrDefaultAsync(t => t.Name == "3 Karty");
+            var scratchType = await _context.GameTypes.FirstOrDefaultAsync(t => t.Name == "Zdrapka");
+
+            _context.Games.Add(new Game { Id = Guid.NewGuid(), Name = "Ślepy Los", Description = "Wybierz 1 z 3 kart. Co kryje reszta?", Cost = 15, GameTypeId = cardsType.Id });
+            _context.Games.Add(new Game { Id = Guid.NewGuid(), Name = "Szczęśliwa Zdrapka", Description = "Kup i zdrap pole!", Cost = 5, GameTypeId = scratchType.Id });
+            await _context.SaveChangesAsync();
         }
     }
+}
