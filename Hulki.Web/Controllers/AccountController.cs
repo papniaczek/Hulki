0f66ab14cd0a52768beea98440b4d0cb408b1ea5
xlennyi <koxamipl@gmail.com>
2026-05-29 16:26:16 +0200

    fixing bugs and make web look better

--- Hulki.Web/Controllers/AccountController.cs
+++ Hulki.Web/Controllers/AccountController.cs
@@ -1,66 +1,87 @@
+using System.Threading.Tasks;
 using Hulki.Web.Data;
 using Hulki.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace Hulki.Web.Controllers;
 
 public class AccountController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly ApplicationDbContext _context;
+
+    public AccountController(
+        UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager,
+        ApplicationDbContext context
+    )
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly ApplicationDbContext _context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _context = context;
+    }
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+    [HttpGet]
+    public IActionResult Register() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Register(
+        string email,
+        string password,
+        string firstName,
+        string lastName
+    )
+    {
+        var user = new AppUser
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _context = context;
-        }
+            UserName = email,
+            Email = email,
+            FirstName = firstName,
+            LastName = lastName,
+        };
+        var result = await _userManager.CreateAsync(user, password);
 
-        [HttpGet]
-        public IActionResult Register() => View();
-
-        [HttpPost]
-        public async Task<IActionResult> Register(string email, string password, string firstName, string lastName)
+        if (result.Succeeded)
         {
-            var user = new AppUser { UserName = email, Email = email, FirstName = firstName, LastName = lastName };
-            var result = await _userManager.CreateAsync(user, password);
+            var wallet = new Wallet { AppUserId = user.Id, Balance = 0 };
+            _context.Wallets.Add(wallet);
+            await _context.SaveChangesAsync();
 
-            if (result.Succeeded)
-            {
-                var wallet = new Wallet { AppUserId = user.Id, Balance = 0 };
-                _context.Wallets.Add(wallet);
-                await _context.SaveChangesAsync();
-
-                await _signInManager.SignInAsync(user, isPersistent: false);
-                return RedirectToAction("Index", "Home");
-            }
-            
-            foreach (var error in result.Errors)
-                ModelState.AddModelError(string.Empty, error.Description);
-
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Login() => View();
-
-        [HttpPost]
-        public async Task<IActionResult> Login(string email, string password)
-        {
-            var result = await _signInManager.PasswordSignInAsync(email, password, isPersistent: false, lockoutOnFailure: false);
-            if (result.Succeeded) return RedirectToAction("Index", "Home");
-
-            ModelState.AddModelError(string.Empty, "Nieprawidłowy email lub hasło.");
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
+            await _signInManager.SignInAsync(user, isPersistent: false);
             return RedirectToAction("Index", "Home");
         }
+
+        foreach (var error in result.Errors)
+            ModelState.AddModelError(string.Empty, error.Description);
+
+        return View();
     }
+
+    [HttpGet]
+    public IActionResult Login() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var result = await _signInManager.PasswordSignInAsync(
+            email,
+            password,
+            isPersistent: false,
+            lockoutOnFailure: false
+        );
+        if (result.Succeeded)
+            return RedirectToAction("Index", "Home");
+
+        ModelState.AddModelError(string.Empty, "Nieprawidłowy email lub hasło.");
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
